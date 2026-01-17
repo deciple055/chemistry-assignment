5118cb7ac5f6732719e714bae2b06fba0875b30b
krishna <deciple055@gmail.com>
2026-01-17 11:20:54 +0530

    final submission

--- Downloads/chem innovative assignment/limesoda.c
+++ Downloads/chem innovative assignment/limesoda.c
@@ -1,6 +1,12 @@
+//
+//  chem.c
+//  Created by krishna makodiya, krishna vishwakarma , krish pahuja , krishna kanojiya, krishna mishra  on 17/01/26.
+
+
+
 #include <stdio.h>
 
-int main() {
+int main(void) {
     float water;
     float CaHCO3, MgHCO3, CaSO4, CaCl2, MgSO4, MgCl2;
     float CaHCO3_eq, MgHCO3_eq, CaSO4_eq, CaCl2_eq, MgSO4_eq, MgCl2_eq;
@@ -52,44 +58,75 @@ int main() {
 
         lime = (tempHard + mgPermHard) * 0.74 * water / 1000;
         soda = permHard * 1.06 * water / 1000;
+        
+        totalHard = tempHard + permHard;
+
+        printf("\n----- WATER QUALITY ANALYSIS -----\n");
+        printf("Temporary Hardness : %.2f ppm\n", tempHard);
+        printf("Permanent Hardness : %.2f ppm\n", permHard);
+        printf("Total Hardness     : %.2f ppm\n", totalHard);
+
+        printf("\n----- TREATMENT DECISION -----\n");
+
+        if (totalHard <= 300) {
+            printf("Water is fit for drinking.\n");
+            printf("No softening process required.\n");
+        }
+        else if (totalHard > 300 && totalHard <= 600) {
+            printf("Water is hard.\n");
+            printf("Selected Process: Zeolite Process\n");
+            printf("Reason: Effective for removal of permanent hardness.\n");
+        }
+        else {
+            printf("Water is very hard.\n");
+            printf("Selected Process: Lime-Soda Process\n");
+            printf("Reason: Economical and suitable for highly hard water.\n");
+        }
+
+        printf("\n----- CHEMICAL REQUIREMENT -----\n");
+        printf("Lime required (Ca(OH)2): %.3f kg\n", lime);
+        printf("Soda required (Na2CO3) : %.3f kg\n", soda);
     }
-    else {
+    else if(known == 'Y' || known == 'y') {
         printf("\nEnter Temporary Hardness (ppm): ");
         scanf("%f", &tempHard);
 
         printf("Enter Permanent Hardness (ppm): ");
         scanf("%f", &permHard);
-    }
+        
+        
+        totalHard = tempHard + permHard;
 
-    totalHard = tempHard + permHard;
+        printf("\n----- WATER QUALITY ANALYSIS -----\n");
+        printf("Temporary Hardness : %.2f ppm\n", tempHard);
+        printf("Permanent Hardness : %.2f ppm\n", permHard);
+        printf("Total Hardness     : %.2f ppm\n", totalHard);
 
-    printf("\n----- WATER QUALITY ANALYSIS(Krishna Vishwakarma) -----\n");
-    printf("Temporary Hardness : %.2f ppm\n", tempHard);
-    printf("Permanent Hardness : %.2f ppm\n", permHard);
-    printf("Total Hardness     : %.2f ppm\n", totalHard);
+        printf("\n----- TREATMENT DECISION -----\n");
 
-    printf("\n----- TREATMENT DECISION -----\n");
-
-    if (totalHard <= 300) {
-        printf("Water is fit for drinking.\n");
-        printf("No softening process required.\n");
-    }
-    else if (totalHard > 300 && totalHard <= 600) {
-        printf("Water is hard.\n");
-        printf("Selected Process: Zeolite Process\n");
-        printf("Reason: Effective for removal of permanent hardness.\n");
+        if (totalHard <= 300) {
+            printf("Water is fit for drinking.\n");
+            printf("No softening process required.\n");
+        }
+        else if (totalHard > 300 && totalHard <= 600) {
+            printf("Water is hard.\n");
+            printf("Selected Process: Zeolite Process\n");
+            printf("Reason: Effective for removal of permanent hardness.\n");
+        }
+        else {
+            printf("Water is very hard.\n");
+            printf("Selected Process: Lime-Soda Process\n");
+            printf("Reason: Economical and suitable for highly hard water.\n");
+        }
     }
     else {
-        printf("Water is very hard.\n");
-        printf("Selected Process: Lime-Soda Process\n");
-        printf("Reason: Economical and suitable for highly hard water.\n");
+        printf("\n \t----PLEASE ENTER Y/N----\n");
     }
+    
 
-    if (known == 'N' || known == 'n') {
-        printf("\n----- CHEMICAL REQUIREMENT -----\n");
-        printf("Lime required (Ca(OH)2): %.3f kg\n", lime);
-        printf("Soda required (Na2CO3) : %.3f kg\n", soda);
-    }
+    
+    printf("\n----- THIS ASSIGNMENT BELONGS TO GROUP THREE ! -----\n \t-----FROM S.no 13-TO-18 -----\n");
+    printf(" 13-->krish pahuja \n 14-->krishna kannojiya \n 15-->krishna kumar mishra\n 16-->Krishna makodiya \n 17-->krishna patel \n 18-->krishna vishwakarma\n");
 
     return 0;
 }
